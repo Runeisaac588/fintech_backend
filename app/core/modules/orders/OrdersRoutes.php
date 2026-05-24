6b18ed93d95e8958b3fa92ae951f00098c6bd70a
Runeisaac588 <adanisakito@gmail.com>
2026-05-23 18:19:55 -0600

    arreglando nombre namespace de orders

--- app/core/modules/orders/OrdersRoutes.php
+++ app/core/modules/orders/OrdersRoutes.php
@@ -2,7 +2,7 @@
 namespace App\Core\Modules\Orders;
 
 use Illuminate\Support\Facades\Route;
-use App\Core\Modules\orders\OrdersController;
+use App\Core\Modules\Orders\OrdersController;
 
 Route::prefix('orders')->group(function () {
     Route::get('/', [OrdersController::class, 'index']);
